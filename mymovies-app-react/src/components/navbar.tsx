57d475c8fb4e7bbb93a0228e83b78becaeca4660
Agung Cahya <agungcahya122@gmail.com>
2023-01-04 07:47:36 +0700

    menambahkan feature search, profil, logoImage pada navbar

--- mymovies-app-react/src/components/navbar.tsx
+++ mymovies-app-react/src/components/navbar.tsx
@@ -3,8 +3,91 @@ import React, { Component } from "react";
 export default class Navbar extends Component {
   render() {
     return (
-      <div className="navbar bg-base-100 sticky-top-0">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Home</a>
+              </li>
+              <li>
+                <a>Favorite</a>
+              </li>
+              <li>
+                <a>Anime Rekomendasi</a>
+              </li>
+              <li>
+                <a>Jadwal Update</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-center">
+          <img src="../assets/logo-nav.png" alt="logo-nav.png" />
+        </div>
+
+        <div className="navbar-end">
+          <div className="form-control">
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered"
+            />
+          </div>
+          <button className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+          <div className="dropdown dropdown-end">
+            <label
+              tabIndex={0}
+              className="btn btn-ghost btn-circle avatar mr-3"
+            >
+              <div className="w-10 rounded-full">
+                <img src="https://placeimg.com/80/80/people" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 shadow p-1 bg-base-100 rounded-box w-30"
+            >
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
