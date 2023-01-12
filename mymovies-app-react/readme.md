10aeb0d7c401b29ec95b34fea5419e513d10e760
Agung Cahya <agungcahya122@gmail.com>
2023-01-12 17:11:01 +0700

    menambahkan readme.md

---
+++ mymovies-app-react/readme.md
@@ -0,0 +1,3 @@
+Merupakan website menampilkan tayangan film yang sedang ada dan terbaru, terdapat halaman detail dan favorit fungsi untuk memberikan kemudahan dalam pemilihan film yang akan ditonton,
+
+berikut link yang bisa digunakan untuk mengunjungi halaman web tersebut : https://mymovies-app-react-pearl.vercel.app
---
+++ mymovies-app-react/src/components/switcher.tsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+
+const SwitchDark = () => {
+  return (
+    <button className="btn btn-ghost btn-circle">
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="sun cursor-pointer h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="moon h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h16M4 18h7"
+          />
+        </svg>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+          />
+        </svg>
+      </div>
+    </button>
+  );
+};
+
+export default SwitchDark;
